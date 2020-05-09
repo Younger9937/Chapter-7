fb36e48d3e8d7e3421782946cd208e84eeb886e5
Younger9937 <2437156099@qq.com>
2020-05-09 17:17:57 +0800

    Homework

--- assignment/VideoPlayer/app/src/main/java/com/bytedance/videoplayer/MainActivity.java
+++ assignment/VideoPlayer/app/src/main/java/com/bytedance/videoplayer/MainActivity.java
@@ -1,19 +1,78 @@
 package com.bytedance.videoplayer;
 
+
+import android.content.res.Configuration;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.ImageView;
+import android.util.Log;
+import android.view.View;
+import android.widget.MediaController;
+import android.widget.RelativeLayout;
+import android.widget.VideoView;
 
-import com.bumptech.glide.Glide;
+public class MainActivity extends AppCompatActivity  {
 
-public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
+    private VideoView videoView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG,"onCreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ImageView imageView = findViewById(R.id.imageView);
-        String url = "https://s3.pstatp.com/toutiao/static/img/logo.271e845.png";
-        Glide.with(this).load(url).into(imageView);
+
+        videoView = findViewById(R.id.videoView);
+
+        Uri uri = getIntent().getData();
+        Log.d(TAG,"uri:"+uri);
+        videoView.setVideoURI(uri);
+
+        MediaController mc = new MediaController(this);
+        mc.setVisibility(View.VISIBLE);
+        videoView.setMediaController(mc);
+
+        RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.MATCH_PARENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT
+        );
+        videoView.setLayoutParams(layoutParams);
     }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        Log.d(TAG, "onConfigurationChanged:"+newConfig);
+        if(newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE){
+            Log.d(TAG,"横屏");
+            RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(
+                    RelativeLayout.LayoutParams.FILL_PARENT,
+                    RelativeLayout.LayoutParams.FILL_PARENT
+            );
+
+            layoutParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
+            layoutParams.addRule(RelativeLayout.ALIGN_PARENT_TOP);
+            layoutParams.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
+            layoutParams.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
+            videoView.setLayoutParams(layoutParams);
+        }
+        else{
+            if(newConfig.orientation == Configuration.ORIENTATION_PORTRAIT){
+                Log.d(TAG,"竖屏");
+
+                RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(
+                        RelativeLayout.LayoutParams.MATCH_PARENT,
+                        RelativeLayout.LayoutParams.WRAP_CONTENT
+                );
+                videoView.setLayoutParams(layoutParams);
+            }
+        }
+        super.onConfigurationChanged(newConfig);
+    }
+
+    private String getVideoPath(int resId) {
+        return "android.resource://" + this.getPackageName() + "/" + resId;
+    }
+
+
 }
